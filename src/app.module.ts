ee03e017131fd7bc4a80b103d4279a98a87e223c
Thainan Luiz <thainandj@hotmail.com>
2024-09-28 16:29:06 -0300

    refactor: remove firebase from app
    chore: change docker-compose.yml

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,14 @@ import { MiddlewareConsumer, Module, RequestMethod } from "@nestjs/common";
 import { ConfigModule } from "@nestjs/config";
 import { LoggerMiddleware } from "./middleware/logger.middleware";
 import { MeasurementModule } from "./module/measurement/measurement.module";
+import { MulterModule } from "@nestjs/platform-express";
 
 @Module({
-	imports: [ConfigModule.forRoot(), MeasurementModule],
+	imports: [
+		ConfigModule.forRoot(),
+		MulterModule.register({ dest: "/assets/uploads" }),
+		MeasurementModule,
+	],
 })
 export class AppModule {
 	configure(consumer: MiddlewareConsumer) {
